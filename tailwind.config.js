2a890ffeaf1e7143d5e7e6f4dbeeab319319f471
SIM VIBOL <vsim9732@gmail.com>
2025-12-23 14:47:21 -1200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fb8a47",
+        secondary: "#ffa448",
+        dark: "#1e1e1e",
+        light: "#f5f5f5",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
